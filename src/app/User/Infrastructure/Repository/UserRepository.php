9b71b7754cc70c42e0e7b9e4c095cd74310819e1
Application-drop-up <msts.oo0131@gmail.com>
2025-05-13 20:17:28 +0900

    refactor: Inject PasswordHasherInterface into UserRepository

--- src/app/User/Infrastructure/Repository/UserRepository.php
+++ src/app/User/Infrastructure/Repository/UserRepository.php
@@ -7,23 +7,26 @@ use App\User\Domain\Entity\UserEntity;
 use App\User\Domain\Factory\UserFromModelEntityFactory;
 use App\User\Domain\RepositoryInterface\UserRepositoryInterface;
 use App\User\Domain\ValueObject\Email;
-use InvalidArgumentException;
+use App\User\Domain\RepositoryInterface\PasswordHasherInterface;
+use LogicException;
 
 class UserRepository implements UserRepositoryInterface
 {
+    public function __construct(
+        private readonly PasswordHasherInterface $hasher
+    ) {}
+
     public function save(UserEntity $entity): ?UserEntity
     {
-        $notValidatedEmail = $this->existsByEmail($entity->getEmail());
-
-        if ($notValidatedEmail) {
-            throw new InvalidArgumentException('Email is already in use.');
-        }
+        $hashed = $entity->getPassword()
+            ? $entity->getPassword()->getHashedPassword()
+            : throw new LogicException('Password is missing.');
 
         $model = User::create([
             'first_name' => $entity->getFirstName(),
             'last_name' => $entity->getLastName(),
             'email' => $entity->getEmail()->getValue(),
-            'password' => $entity->getValidatedHashedPassword($entity),
+            'password' => $hashed,
             'bio' => $entity->getBio(),
             'location' => $entity->getLocation(),
             'skills' => json_encode($entity->getSkills()),
