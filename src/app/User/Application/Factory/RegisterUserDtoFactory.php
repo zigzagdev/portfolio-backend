927f0f0d100905a3420bba8abf1d713a51e8d5c2
Application-drop-up <msts.oo0131@gmail.com>
2025-05-17 16:53:47 +0900

    fix: fix-user-dto

--- src/app/User/Application/Factory/RegisterUserDtoFactory.php
+++ src/app/User/Application/Factory/RegisterUserDtoFactory.php
@@ -2,28 +2,24 @@
 
 namespace App\User\Application\Factory;
 
-use App\Models\User;
+use App\User\Domain\Entity\UserEntity;
 use App\User\Domain\ValueObject\Email;
 use App\Common\Domain\UserId;
 use App\User\Application\Dto\RegisterUserDto;
 
 class RegisterUserDtoFactory
 {
-    public static function build(User $user): RegisterUserDto
+    public static function build(UserEntity $entity): RegisterUserDto
     {
-        $resultArray = $user->toArray();
-
         return new RegisterUserDto(
-            new UserId($resultArray['id']),
-            $resultArray['first_name'],
-            $resultArray['last_name'],
-            new Email($resultArray['email']),
-            $resultArray['bio'] ?? null,
-            $resultArray['location'] ?? null,
-            is_string($resultArray['skills'])
-                ? json_decode($resultArray['skills'], true)
-                : (is_array($resultArray['skills']) ? $resultArray['skills'] : []),
-            $resultArray['profile_image'] ?? null,
+            new UserId($entity->getUserId()?->getValue()),
+            $entity->getFirstName(),
+            $entity->getLastName(),
+            new Email($entity->getEmail()->getValue()),
+            $entity->getBio(),
+            $entity->getLocation(),
+            $entity->getSkills(),
+            $entity->getProfileImage(),
         );
     }
 }
