0d772089f4e2040dbe07cca2c85f376c66eb0072
Application-drop-up <msts.oo0131@gmail.com>
2025-05-25 15:06:31 +0900

    feat: fix-update-user-entity-test

--- src/app/User/Domain/Factory/UserUpdateEntityFactory.php
+++ src/app/User/Domain/Factory/UserUpdateEntityFactory.php
@@ -2,23 +2,26 @@
 
 namespace App\User\Domain\Factory;
 
+use App\User\Application\UseCommand\UpdateUserCommand;
 use App\User\Domain\Entity\UserEntity;
 use App\Common\Domain\UserId;
 use App\User\Domain\ValueObject\Email;
 
 class UserUpdateEntityFactory
 {
-    public static function build(UserEntity $entity): UserEntity
+    public static function build(
+        UpdateUserCommand $command
+    ): UserEntity
     {
         return new UserEntity(
-            id: new Userid($entity->getUserId()?->getValue()),
-            firstName: $entity->getFirstName(),
-            lastName: $entity->getLastName(),
-            email: new Email($entity->getEmail()->getValue()),
-            bio: $entity->getBio() ?? null,
-            location: $entity->getLocation() ?? null,
-            skills: $entity->getSkills() ?? [],
-            profileImage: $entity->getProfileImage() ?? null,
+            id: new Userid($command->getId()),
+            firstName: $command->getFirstName(),
+            lastName: $command->getLastName(),
+            email: new Email($command->getEmail()),
+            bio: $command->getBio(),
+            location: $command->getLocation(),
+            skills: $command->getSkills(),
+            profileImage: $command->getProfileImage()
         );
     }
 }
