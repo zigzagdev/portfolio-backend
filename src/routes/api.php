523b5b080b145714f58e71417e2378cb81ffb620
Application-drop-up <msts.oo0131@gmail.com>
2025-06-03 19:49:32 +0900

    feat: add-logout-feature-test

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,9 +3,12 @@
 use App\User\Presentation\Controller\UserController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::post('logout', [UserController::class, 'logout'])->name('logout');
+
 Route::prefix('users')->name('user.')->group(function () {
     Route::post('register', [UserController::class, 'createUser'])->name('register');
+    Route::post('login', [UserController::class, 'login'])->name('login');
     Route::get('/show/{id}', [UserController::class, 'showUser'])->name('show');
     Route::put('{id}', [UserController::class, 'update'])->name('update');
-    Route::post('login', [UserController::class, 'login'])->name('login');
 });
