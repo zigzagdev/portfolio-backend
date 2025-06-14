6e35fa39febbd8167e1b88eab7fefc7baec3b7a9
Application-drop-up <msts.oo0131@gmail.com>
2025-06-14 13:49:29 +0900

    feat: refactor-infera-return-value

--- src/app/Post/Infrastructure/QueryService/GetAllUserPostQueryService.php
+++ src/app/Post/Infrastructure/QueryService/GetAllUserPostQueryService.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Post\Application\QueryServiceInterface\GetAllUserPostQueryServiceInterface;
 use App\Post\Domain\Entity\PostEntityCollection;
 use App\Post\Domain\EntityFactory\PostFromModelEntityFactory;
+use App\Common\Application\Dto\Pagination as PaginationDto;
 
 class GetAllUserPostQueryService implements GetAllUserPostQueryServiceInterface
 {
@@ -13,14 +14,48 @@ class GetAllUserPostQueryService implements GetAllUserPostQueryServiceInterface
         private readonly Post $post
     ) {}
 
-    public function getAllUserPosts(int $userId): PostEntityCollection
+    public function getAllUserPosts(
+        int $userId,
+        int $perPage,
+        int $currentPage,
+    ): PaginationDto
     {
-        $userPosts = $this->post->where('user_id', $userId)->get();
+        $userPosts = $this->post
+            ->where('user_id', $userId)
+            ->orderBy('created_at', 'desc')
+            ->paginate(
+                $perPage,
+                ['*'],
+                'page',
+                $currentPage
+            );
 
-        $postEntities = $userPosts->map(function ($post) {
-            return PostFromModelEntityFactory::build($post->toArray());
-        });
+        return $this->paginationDto($userPosts->toArray());
+    }
 
-        return new PostEntityCollection($postEntities->all());
+    private function paginationDto(
+        array $data
+    ): PaginationDto
+    {
+        $dtos = array_map(
+            fn($item) => PostFromModelEntityFactory::build($item),
+            $data['data']
+        );
+
+        return new PaginationDto(
+            $dtos,
+            currentPage: $data['current_page'] ?? null,
+            from: $data['from'] ?? null,
+            to: $data['to'] ?? null,
+            perPage: $data['per_page'] ?? null,
+            path: $data['path'] ?? null,
+            lastPage: $data['last_page'] ?? null,
+            total: $data['total'] ?? null,
+            firstPageUrl: $data['first_page_url'] ?? null,
+            lastPageUrl: $data['last_page_url'] ?? null,
+            nextPageUrl: $data['next_page_url'] ?? null,
+            prevPageUrl: $data['prev_page_url'] ?? null,
+            links: $data['links'] ?? null
+        );
     }
 }
