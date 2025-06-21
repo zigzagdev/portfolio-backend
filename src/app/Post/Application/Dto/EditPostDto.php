d04b37bf1b41a5209e9c1e7e2f4bd3f00876ace5
Application-drop-up <msts.oo0131@gmail.com>
2025-06-21 16:06:13 +0900

    feat: add-edit-post-dto

---
+++ src/app/Post/Application/Dto/EditPostDto.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Post\Application\Dto;
+
+use App\Common\Domain\ValueObject\PostId;
+use App\Common\Domain\ValueObject\UserId;
+use App\Post\Domain\Entity\PostEntity;
+use App\Post\Domain\ValueObject\Postvisibility;
+
+class EditPostDto
+{
+    public function __construct(
+        public readonly PostEntity $entity,
+    ) {
+    }
+
+    public function getId(): PostId
+    {
+        return $this->entity->getId();
+    }
+
+    public function getUserid(): UserId
+    {
+        return $this->entity->getUserId();
+    }
+
+    public function getContent(): string
+    {
+        return $this->entity->getContent();
+    }
+
+    public function getMediaPath(): ?string
+    {
+        return $this->entity->getMediaPath();
+    }
+
+    public function getVisibility(): PostVisibility
+    {
+        return $this->entity->getPostVisibility();
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'userId' => $this->getUserid()->getValue(),
+            'content' => $this->getContent(),
+            'mediaPath' => $this->getMediaPath(),
+            'visibility' => $this->getVisibility()->getValue(),
+        ];
+    }
+
+    public static function build(PostEntity $entity): EditPostDto
+    {
+        return new EditPostDto(
+            entity: $entity
+        );
+    }
+}
