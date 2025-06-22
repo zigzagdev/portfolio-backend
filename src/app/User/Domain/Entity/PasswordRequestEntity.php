eb362e937097ef45c4620922440d8633242bf485
Application-drop-up <msts.oo0131@gmail.com>
2025-06-22 18:42:11 +0900

    feat: add-password-reset-entity

---
+++ src/app/User/Domain/Entity/PasswordRequestEntity.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\User\Domain\Entity;
+
+use App\Common\Domain\ValueObject\ExpiredAt;
+use App\Common\Domain\ValueObject\UserId;
+use App\User\Domain\ValueObject\PasswordResetToken;
+use DateTimeImmutable;
+use InvalidArgumentException;
+
+class PasswordRequestEntity
+{
+    public function __construct(
+        private readonly int $id,
+        private readonly UserId $userId,
+        private readonly PasswordResetToken $token,
+        private readonly DateTimeImmutable $requestedAt,
+        private readonly ExpiredAt $expiredAt
+    )
+    {
+        if (empty($this->token->getValue())) {
+            throw new InvalidArgumentException('Token cannot be empty.');
+        }
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): UserId
+    {
+        return $this->userId;
+    }
+
+    public function getToken(): PasswordResetToken
+    {
+        return $this->token;
+    }
+
+    public function getRequestedAt(): DateTimeImmutable
+    {
+        return $this->requestedAt;
+    }
+
+    public function getExpiredAt(): ExpiredAt
+    {
+        return $this->expiredAt;
+    }
+
+    public function isExpired(?DateTimeImmutable $now = null): bool
+    {
+        return $this->expiredAt->isExpired($now);
+    }
+}
